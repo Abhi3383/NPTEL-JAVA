e9993d4fdb2a3b560971518f831dfcac63512c3b
Abhi3383 <97604021+Abhi3383@users.noreply.github.com>
2022-07-14 20:17:59 +0530

    Create DemoClass.java

---
+++ WEEK 1/Generic Methods/DemoClass.java
@@ -0,0 +1,15 @@
+public class DemoClass {
+    // Defining a generic method to print any data type
+    <T> void genericPrint(T t){
+        System.out.println(t);
+    }
+    public static void main(String[] args) {
+        DemoClass aObj = new DemoClass();
+        aObj.genericPrint(101);
+        aObj.genericPrint("Joy with java");
+        aObj.genericPrint(3.142);
+
+    }
+}
+Footer
+© 2022 GitHu
